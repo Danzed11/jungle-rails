f940cfd977df688910d26cfc3789a4d4c10bc384
Dan <dan.zajac@mac.com>
2019-06-14 22:50:52 +0000

    Fixed POST create of reviews in the ReviewsController

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,19 @@
+class ReviewsController < ApplicationController
+  def create
+    @review = Review.new(review_params)
+    @review.user_id = current_user.id
+    @review.product_id = params[:product_id]
+    puts "~~~~~~~I AM HERE~~~~~~~", @review.inspect
+
+    if @review.save
+      redirect_to '/'
+    else
+      redirect_to :back
+    end
+  end
+
+ private
+  def review_params
+    params.require(:review).permit(:description, :rating)
+  end
+end
