984681aa58b269f5a9ebd744a823ddb3535a1379
Dan <dan.zajac@mac.com>
2019-06-18 18:38:33 +0000

    Wrote test code for product and half of User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :reviews
+
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: { case_sensitive: false } 
+
 end
